2abd7a0cb1dd3ed05ff405c205f3e94a9a2401ce
viruzvz <viruzvz@gmail.com>
2020-05-21 21:31:28 -0300

    first commit

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import './styles/main.scss'
+import './styles/main.less'
+import 'popper.js'
+import 'bootstrap'
+import 'babel-polyfill'
+// import 'ekko-lightbox'
+// import Stickyfill from 'stickyfilljs'
+import $ from 'jquery'
+window.jQuery = $
+window.$ = $
+
