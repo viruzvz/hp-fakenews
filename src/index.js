c59d44f1313943fd865d6531f7a61455c91f7de5
viruzvz <viruzvz@gmail.com>
2020-06-26 13:17:30 -0300

    projeto finalizado

--- src/index.js
+++ src/index.js
@@ -3,32 +3,10 @@ import './styles/main.less'
 import 'popper.js'
 import 'bootstrap'
 import 'babel-polyfill'
-import nanoScroller from 'nanoscroller'
-import 'ekko-lightbox'
-// import Stickyfill from 'stickyfilljs'
 import $ from 'jquery'
 window.jQuery = $
 window.$ = $
-
-// Js-fixnano serve para corrigir problema de conflito com o bootstrap dropdown, sem isso ele náo aparece dentro do dropdown
-$(".nano").nanoScroller()
-$('.js-fixnano').click(function () {
-  setTimeout(function () {
-    $(".nano").nanoScroller()
-  }, 100)
-})
-// Focus no input search quando executa collapse
-$('#collapseExample').on('shown.bs.collapse', function () {
-  $('.js-trigger').trigger('focus')
-})
-// Fecha collapse clicando fora do target, clicando na tela. 
-$(document).click(function(e) {
-	if (!$(e.target).is('.js-closeoutside')) {
-    	$('.js-collapse-search').collapse('hide')   
-    }
-})
-// Lightbox, efeito para abrir fotos e videos
-$(document).on('click', '[data-toggle="lightbox"]', function (event) {
-  event.preventDefault()
-  $(this).ekkoLightbox()
-})
+$('#myTabs a').click(function (e) {
+    e.preventDefault()
+    $(this).tab('show')
+  })
